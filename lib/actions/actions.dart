b21ef6c00e46e5859affe0423af9bb92a8c11635
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-09 18:12:55 +0000

    Updating to latest FlutterFlow output.

--- lib/actions/actions.dart
+++ lib/actions/actions.dart
@@ -5,6 +5,7 @@ import '/backend/backend.dart';
 import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
 
 Future updateAppStateAuthUser(BuildContext context) async {
@@ -34,5 +35,29 @@ Future updateAppStateAuthUser(BuildContext context) async {
           ).toString().toString(),
       );
     });
+  }
+}
+
+Future loadCountriesToAppState(BuildContext context) async {
+  ApiCallResponse? jsonCountryList;
+  List<CountrieStruct>? countrieData;
+
+  if (FFAppState().countries.first == null) {
+    jsonCountryList = await GetdataGroup.datalistCall.call(
+      contentType: 'Country',
+      fields: 'RuName,FlagLinkH24',
+    );
+    if ((jsonCountryList?.succeeded ?? true)) {
+      countrieData = await actions.jsonToDataTypeCountrie(
+        getJsonField(
+          (jsonCountryList?.jsonBody ?? ''),
+          r'''$.list''',
+          true,
+        ),
+      );
+      FFAppState().update(() {
+        FFAppState().countries = countrieData!.toList().cast<CountrieStruct>();
+      });
+    }
   }
 }
