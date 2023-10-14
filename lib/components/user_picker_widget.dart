be24e26d32f2428f65f3f68ec0cae307a1d0d68a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-14 14:38:18 +0000

    Updating to latest FlutterFlow output.

--- lib/components/user_picker_widget.dart
+++ lib/components/user_picker_widget.dart
@@ -849,21 +849,16 @@ class _UserPickerWidgetState extends State<UserPickerWidget> {
                                           );
                                           if ((_model.apiResultvoz?.succeeded ??
                                               true)) {
-                                            await showDialog(
-                                              context: context,
-                                              builder: (alertDialogContext) {
-                                                return AlertDialog(
-                                                  title: Text('1'),
-                                                  actions: [
-                                                    TextButton(
-                                                      onPressed: () =>
-                                                          Navigator.pop(
-                                                              alertDialogContext),
-                                                      child: Text('Ok'),
-                                                    ),
-                                                  ],
-                                                );
-                                              },
+                                            await UpdatedataGroup.userteamCall
+                                                .call(
+                                              contentType: 'User',
+                                              tag: 'false',
+                                              teamRole: 'Вне команды',
+                                              updatedAt: getCurrentTimestamp
+                                                  .toString(),
+                                              contentId:
+                                                  _model.selectedUserIdINT,
+                                              lineUp: 'false',
                                             );
                                             setState(() {
                                               FFAppState()
