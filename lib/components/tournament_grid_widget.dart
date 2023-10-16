82d94824c921e0e1df8d02427161640691746b56
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-16 11:28:24 +0000

    Updating to latest FlutterFlow output.

--- lib/components/tournament_grid_widget.dart
+++ lib/components/tournament_grid_widget.dart
@@ -135,7 +135,7 @@ class _TournamentGridWidgetState extends State<TournamentGridWidget> {
                                         borderRadius:
                                             BorderRadius.circular(3.0),
                                         child: Image.network(
-                                          round1Item.rival1Logo[0],
+                                          round1Item.rival1Logo.first,
                                           width: 22.0,
                                           height: 22.0,
                                           fit: BoxFit.cover,
@@ -202,7 +202,7 @@ class _TournamentGridWidgetState extends State<TournamentGridWidget> {
                                     child: ClipRRect(
                                       borderRadius: BorderRadius.circular(3.0),
                                       child: Image.network(
-                                        round1Item.rival2Logo[0],
+                                        round1Item.rival2Logo.first,
                                         width: 22.0,
                                         height: 22.0,
                                         fit: BoxFit.cover,
